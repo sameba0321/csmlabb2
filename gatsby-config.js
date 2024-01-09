151f13d315f5b636efe0b2fae4d7e7f00f4cb2c8
sameba0321 <sam.ebadeh.ahwazi@iths.se>
2024-01-09 13:23:17 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "4m0n1uen1i0Rg-JtoH7mBZgKDGG4bnio7eYj-_v50aE",
+      "spaceId": "whtchnntgqs4"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
